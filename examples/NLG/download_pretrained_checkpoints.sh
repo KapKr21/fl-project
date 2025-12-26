bb3b86719afe61d72a037ede2e68fee323fc9ce2
Kapil K <kapilnotsharma@kapils-air.speedport.ip>
2025-12-26 21:23:55 +0100

    Edit: Changes to moving to Google collab for GPU.

--- examples/NLG/download_pretrained_checkpoints.sh
+++ examples/NLG/download_pretrained_checkpoints.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
+#MacOS changes for GPT2-medium - 354.92M
 echo "downloading pretrained model checkpoints..."
 mkdir pretrained_checkpoints
 cd pretrained_checkpoints
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-pytorch_model.bin
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-large-pytorch_model.bin
+
+#wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin
+
+curl -L -O https://huggingface.co/gpt2-medium/resolve/main/pytorch_model.bin
+mv pytorch_model.bin gpt2-medium-pytorch_model.bin
+
+#wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-large-pytorch_model.bin
+
 cd ..
 
 echo "script complete!"
